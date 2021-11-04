ecb97b3a8b620fc44ed2b9c7b11efb0f508f65b8
jon wong <j@jnwng.com>
2021-11-04 00:19:15 -0400

    orcanauts

--- src/config.js
+++ src/config.js
@@ -30,13 +30,12 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "background", options: { bypassDNA: true } },
+      { name: "body" },
+      { name: "eyes" },
+      { name: "mouth" },
+      { name: "hat" },
+      { name: "accessory" },
     ],
   },
 ];
